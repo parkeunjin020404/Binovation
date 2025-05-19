c676c804699a44329b8ea653cb4e269caab785b0
parkeunjin020404 <eunjin020404@naver.com>
2025-05-19 20:22:09 +0900

    url 수정

--- trash/urls.py
+++ trash/urls.py
@@ -4,10 +4,10 @@ from .views import *
 urlpatterns = [
     path('post/', TrashStatusView.as_view(), name='trash_status_post'),
     path('weekly-average/<str:device_name>/', DeviceWeeklyAverageView.as_view(), name='device-weekly-average'),
-    path('yesterday-hourly/<str:device_name>/', HourlyStatsYesterdayView.as_view(), name='yesterday-hourly'),
     path('weekly-average/', WeeklyAverageAllDevicesView.as_view(), name='weekly-average-all'),
     path('latest-all/', LatestStatusAllDevicesView.as_view(), name='latest-all'),
     path('yesterday-hourly/all/', HourlyStatsYesterdayAllDevicesView.as_view(), name='hourly-stats-all'),
     path('route/<str:device_name>/', RouteRecommendationView.as_view(), name='route'),
+    path('yesterday-hourly/<str:device_name>/', HourlyStatsYesterdayView.as_view(), name='yesterday-hourly'),
     path('<str:device_name>/', TrashStatusLatestView.as_view(), name='latest_status'),
 ]   
